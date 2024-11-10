00260604598c7950e9b476c876ecc82ad44cd2f0
Carlos <carlosenm.03@gmail.com>
2024-11-10 17:46:41 -0500

    Adding a background change color function to make it more attractive

--- js/Script.js
+++ js/Script.js
@@ -1,8 +1,7 @@
 
-
-
 function FormSubmit(event) {
     event.preventDefault();
+    
     //creating the object
     const orderDetails = {};
     
@@ -20,9 +19,8 @@ function FormSubmit(event) {
 function ShowAlert(objectFromForm){
     
     
-    //getting the values from my object  in a string to sava them in a variable
-    let greeting = `Hi,${objectFromForm.name} we sent the recept to ${objectFromForm.email} about your Order Details, You selected a ${objectFromForm.Size} ${objectFromForm.Flavor} Smoothie 
-    .`
+    //getting the values from my object  in a string to save them in a variable
+    let greeting = `Hi,${objectFromForm.name} we sent the recept to ${objectFromForm.email} about your Order Details, You selected a ${objectFromForm.Size} ${objectFromForm.Flavor} Smoothie .`
     
     //display the variable freeting
     alert(greeting);
@@ -31,3 +29,39 @@ function ShowAlert(objectFromForm){
 
 //event listener when user press submit
 document.getElementById("smoothieForm").addEventListener("submit", FormSubmit);
+
+//------------------------------------------------------------------------------------
+
+//Extra changes
+
+//selecting the field we will used by id
+const selectedElement = document.querySelector("#Smothie");
+
+//when the user change between option an event will happen, and will change the backgound color depeding on the element selected
+selectedElement.addEventListener("change", function() {
+
+    //getting value from the selected field
+    smothieColor= selectedElement.value;
+
+    if (smothieColor == "Strawberry"){
+        document.body.style.backgroundColor = "pink";
+    }
+    else if (smothieColor == "Chocolate") {
+        document.body.style.backgroundColor = "brown";
+        
+    } else if (smothieColor =="Cookie") {
+        document.body.style.backgroundColor = "sandybrown";
+        
+    }
+  });
+
+
+
+// Resources Used for my javascript
+
+// For object creation: https://www.w3schools.com/js/js_object_definition.asp
+
+// To get values from the form https://www.w3schools.com/js/js_validation.asp
+
+//The feature change event, to be able to change color of the background when user select an element. 
+//I got it this site: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/change_event
